13309d4e576cd9d70cb50b6ba35342cf0249dca3
Milagros Luz Soler <milagrosluz06@gmail.com>
2024-02-15 22:51:01 -0300

    cambio footer

--- register.php
+++ register.php
@@ -129,23 +129,44 @@ include_once('guest.php');
                 <section class="flex-conect">
                     <p>SIGAMOS CONECTADOS</p>
                     <nav>
-                        <ul>
-                            <a href="https://www.instagram.com/scagliaclothing"><li><svg class="icons2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></li></a>
-                            <a href="https://www.facebook.com/scagliaclothing"><li><svg class="icons2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg></li></a>
-                            <a href="mailto:scagliaclothing@gmail.com?Subject=Consulta%20-%20web"><li><svg class="icons2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M0 128C0 92.65 28.65 64 64 64H448C483.3 64 512 92.65 512 128V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V128zM48 128V150.1L220.5 291.7C241.1 308.7 270.9 308.7 291.5 291.7L464 150.1V127.1C464 119.2 456.8 111.1 448 111.1H64C55.16 111.1 48 119.2 48 127.1L48 128zM48 212.2V384C48 392.8 55.16 400 64 400H448C456.8 400 464 392.8 464 384V212.2L322 328.8C283.6 360.3 228.4 360.3 189.1 328.8L48 212.2z"/></svg></li></a>
-                        </ul>
+                    <ul>
+                                    <a href="https://www.instagram.com/scagliaclothing"><li><img src="Multimedia\Iconos\instagram.png"></li></a>
+                                    <a href="https://www.facebook.com/scagliaclothing"><li><img src="Multimedia\Iconos\facebook.png"></li></a>
+                                    <a href="mailto:scagliaclothing@gmail.com?Subject=Consulta%20-%20web"><li><img src="Multimedia\Iconos\email.png"></li></a>
+                                </ul>
                     </nav>
                 </section>
 
                 <section class="flex-categ">
-                    <p>MY SCAGLIA</p>
-                    <nav>
-                    <ul>
-                        <a href="Login.php"><li>INICIAR SESIÓN</li></a>
-                        <a href="Register.php"><li>REGISTRARME</li></a>
-                    </ul>
-                    </nav>
-                </section>
+			<?php if(!empty($_SESSION['user'])){
+                if($_SESSION["id_user_role"] == 2) {?>
+				<p>MY SCAGLIA</p>
+                            <nav>
+                            <ul>
+                                <a href="Mi_cuenta.php"><li>MI CUENTA</li></a>
+                            </ul>
+                            </nav>
+                        </section>
+				<?php }else if($_SESSION["id_user_role"] == 1){?>
+				<p>MY SCAGLIA</p>
+                            <nav>
+                            <ul>
+                                <a href="Admin_inicio.php"><li>ADMINISTRADOR</li></a>
+                            </ul>
+                            </nav>
+                        </section>
+			<?php }else{ ?>
+                <p>MY SCAGLIA</p>
+                <nav>
+                <ul>
+                    <a href="Login.php"><li>INICIAR SESION</li></a>
+                    <a href="Register.php"><li>REGISTRARME</li></a>
+                </ul>
+                </nav>
+            </section>
+            <?php }
+            }
+			?>
 
                 <section class="flex-pagos">
                     <hr style="width:100%" color="white" >
